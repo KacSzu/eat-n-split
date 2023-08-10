ea4713e3421953a69b508fcf4c284c62760594a0
KacSzu <szulckacper19@gmail.com>
2023-08-10 12:44:58 +0200

    Building the Static App: Forms

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,15 +22,31 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend(!showAddFriend);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        {showAddFriend && <AddFriendForm />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
 
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
 function FriendsList() {
   const friends = initialFriends;
   return (
@@ -57,10 +75,44 @@ function Friend({ friend }) {
           ? `You owe ${friend.name} ${Math.abs(friend.balance)}€`
           : friend.balance > 0
           ? `${friend.name} owes you ${friend.balance}€`
-          : ""}
+          : null}
       </p>
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function AddFriendForm() {
+  return (
+    <>
+      <form className="form-add-friend">
+        <label>👨‍👦 Friend name </label>
+        <input type="text" placeholder="Your friend name"></input>
+        <label>🖼 Image URL</label>
+        <input type="text" placeholder="Friend's image URL"></input>
+        <Button>Add</Button>
+      </form>
+    </>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Bill value</label>
+      <input type="text" placeholder="Type bill value"></input>
+      <label>🙎‍♂️ Your expenses</label>
+      <input type="text" placeholder="Type your expenses"></input>
+      <label>👨‍👦 X's expenses</label>
+      <input type="text" placeholder="X's expenses" disabled></input>
+      <label>🤑 Who is paying ?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
   );
 }
